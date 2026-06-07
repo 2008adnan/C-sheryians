c793ef3639cc9f8f3ddd60d79c320989c4a44550
2008adnan <2008adnanahmad@gmail.com>
2026-06-07 14:34:50 +0530

    just checking

--- Operators/incdec.c
+++ Operators/incdec.c
@@ -5,5 +5,6 @@ int main() {
     printf("%d\n", ++a); // increments a to 7, then prints 7
     printf("%d\n", a--); // prints 7, then decrements a to 6
     printf("%d\n", --a); // decrements a to 5, then prints 5
+    printf("%d\n", a);   // prints 5
     return 0;
 }
