703ae403d9e04f0234db669a5cdecd0cc9e3f563
2008adnan <2008adnanahmad@gmail.com>
2026-06-07 18:45:00 +0530

    fix: update Q2.c to allow user input for length and breadth

--- data types/Q2.c
+++ data types/Q2.c
@@ -1,16 +1,19 @@
 //take length and breadth as input and print area of rectangle
 #include <stdio.h>
 int main(){
-    int l = 30;
-    int b = 15;
+    int l;
+    int b;
+    printf("enter length: ");
+    scanf("%i",&l);
+    printf("enter breadth: ");
+    scanf("%i",&b);
     int perimeter = 2*(l+b);
     int area = l*b;
 
     printf("the area of rectangle is %i\n",area);
     printf("the perimeter of rectangle is %i\n",perimeter);
 
-    printf("area is %i\n",l*b);
-    printf("perimeter is %i\n", 2*(l+b));
+
 
 
     return 0;
