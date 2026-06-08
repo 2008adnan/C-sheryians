16436621eb9aa084c8ab2fc666e6aa2862db6e00
2008adnan <2008adnanahmad@gmail.com>
2026-06-08 14:24:18 +0530

    Add examples and comments for conditional statements in C
    - Introduced a new file `learnt.c` containing various examples of conditional statements.
    - Included basic if-else structure, if ladder, and if-else ladder with explanations.
    - Provided comments to clarify the behavior of each conditional statement.

---
+++ Understanding Conditional Statements/demo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    if (100<10){
+    printf("butter scotch le ana");
+}else if(10!=10){
+    printf("vanilla lete ana");
+}else if(50>500){
+    printf("chocolate le ana");
+}else{
+    printf("mere paise wapas le ana");
+    return 0;  
+}
+}
+
+
+
+
+   
---
+++ Understanding Conditional Statements/learnt.c
@@ -0,0 +1,59 @@
+ /*if(condition){
+    code to be executed if condition is true
+    }
+    else{
+    code to be executed if condition is false
+    }*/
+
+   /*int age;
+   printf("enter your age:");
+   scanf("%d",&age);
+   if(age>=18){
+    printf("you are eligible to vote");
+   }
+   else{
+    printf("you are not eligible to vote");
+    }
+    return 0;*/
+
+    /*if(condition1){
+    code to be executed if condition1 is true
+    }
+    if(condition2){
+    code to be executed if condition2 is true
+    }
+    if(condition3){
+    code to be executed if condition3 is true
+    }    //if ladder, all conditions are checked separately, even if 
+    condition1 is true, condition2 and condition3 will also be checked*/
+
+    /*if(10>100){
+        printf("Hello Duniya\n"); //this will not be printed as the condition is false
+    }
+    if(10<100){
+        printf("Hello Duniya2\n"); //this will be printed as the condition is true, doesnt matter if the previous condition is true or false, this will be checked and executed
+        }
+    if(10==100){
+        printf("Hello Duniya3\n"); //this will not be printed as the condition is false, even if the previous condition is true, this will be checked and executed
+    }
+    if(10!=100){
+        printf("Hello Duniya4\n"); //this will be printed as the condition is true, even if the previous condition is false, this will be checked and executed
+    }
+    if(100>1000){
+        printf("Hello Duniya5\n"); //this will not be printed as the condition is false, even if the previous condition is true, this will be checked and executed
+    }*/
+
+    /*if(condition1){
+    code to be executed if condition1 is true
+    }
+    else if(condition2){
+    code to be executed only if condition1 is false and condition2 is true
+    }
+    else if(condition3){
+    code to be executed only if condition1 and condition2 are false and condition3 is true
+    }
+    else{
+    code to be executed if all conditions are false
+    }   //if-else ladder, if condition1 is true, then condition2 and condition3 will not be checked, if condition1 is false and condition2 is true, then condition3 will not be checked, if condition1 and condition2 are false and condition3 is true, then the else part will not be executed, and if all conditions are false, then the else part will be executed aur last wala else lagana is not compulsory*/
+
+             
