163d4daa48013938582679b230a217e3a2f2fd82
2008adnan <2008adnanahmad@gmail.com>
2026-06-08 08:30:19 +0530

    refactor: restructure input handling into separate functions for clarity

--- Understanding Header files/practise.c
+++ Understanding Header files/practise.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-int main(){
-    char name[100];
+float m,p,c;
+char name[100];
+void program1(){
     printf("enter the name of student:");
     scanf("%s",name);
     printf("enter marks obtained in maths:");
-    float m;
-    scanf("%f",&m);
-    printf("enter marks obtained in physics:");
-    float p;
-    scanf("%f",&p);
-    printf("enter marks obtained in chemistry:");
-    float c;
-    scanf("%f",&c);
+    scanf(" %f",&m);
+}
+void program2(){
+    printf("enter marks obtained in physics:");    
+    scanf(" %f",&p);
+    printf("enter marks obtained in chemistry:");    
+    scanf(" %f",&c);
+}
+int main(){
+    program1();
+    program2();
     float total = m+p+c;
     float per = (total/300)*100;
+    printf("the name of student is %s\n",name);
     printf("the total marks obtained is %.2f\n",total);
     printf("the percentage obtained is %.2f\n",per);
     return 0;
