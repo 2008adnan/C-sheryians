bfbac1a703325e070ca9b5903cb36d3e403066ca
2008adnan <2008adnanahmad@gmail.com>
2026-06-10 19:15:58 +0530

    Refactor and uncomment code for number comparison and grading programs

--- Conditional Statements/problems on condn stmt/practise.c
+++ Conditional Statements/problems on condn stmt/practise.c
@@ -1,6 +1,7 @@
 //check if two numbers are equal or not
 #include <stdio.h>
-int main(){
+
+/*void program1(){
     float a;
     float b;
     printf("enter two numbers:");
@@ -11,5 +12,42 @@ int main(){
     else{
         printf("the numbers are not equal");
     }
+}*/
+
+//take two numbers from user and print the greatest number among them
+
+/*void program2(){
+    float a;
+    float b;
+    printf("enter two numbers:");
+    scanf("%f %f",&a,&b);
+    if(a>b){
+        printf("the greater number is %f",a);
+    }else if(b>a){
+        printf("the greater number is %f",b);
+    }else{
+        printf("both numbers are equal");
+    }
+}*/
+
+//check if a user has passed of failed
+
+/*void program3(){
+    float m;
+    printf("enter your marks:");
+    scanf("%f", &m);
+    if(m<0 || m>100){
+        printf("Invalid response! enter number between 0 and 100");
+    }else if(m>=33){
+        printf("congrats! you have passed\n");
+    }else{
+        printf("try again next time\n");
+    }
+}*/
+
+
+
+int main(){
+    program2();
     return 0;
 }
