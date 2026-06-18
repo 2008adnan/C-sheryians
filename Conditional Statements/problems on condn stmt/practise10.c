48e485be567aa4cdf1055f560cfd99a96a5b8636
2008adnan <2008adnanahmad@gmail.com>
2026-06-18 18:40:25 +0530

    Refactor code structure for improved readability and maintainability

--- Conditional Statements/problems on condn stmt/practise10.c
+++ Conditional Statements/problems on condn stmt/practise10.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
 int main(){
-    
+    int s1,s2,s3,s4;
+    printf("enter you marks of all the four subjects:");
+    scanf("%d", &s1);
+    scanf(" %d", &s2);
+    scanf(" %d", &s3);
+    scanf(" %d", &s4);
+    int per = (s1+s2+s3+s4)/4;
+    printf("Percentage = %d %%" , per);
 }
