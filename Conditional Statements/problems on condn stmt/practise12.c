8904d69eb44dc4a06b0b1227db759cfa566213c5
2008adnan <2008adnanahmad@gmail.com>
2026-06-18 20:02:41 +0530

    Refactor code structure for improved readability and maintainability

--- Conditional Statements/problems on condn stmt/practise12.c
+++ Conditional Statements/problems on condn stmt/practise12.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main(){
     float m;
-    printf("Enter amount to be paid when no discount applied:");
+    printf("Enter original amount to be paid:");
     scanf("%f", &m);
     if(m<0){
         printf("Enter Valid Amount");
     }else if(m>0 && m<=5000){
-        printf("Amount to be paid: %f",m);
+        printf("Amount to be paid: %.2f",m);
     }else if(m>5000 && m<=7000){
         float discount = 0.05*m;
         float pay = m-discount;
-        printf("The amount to be payed after applying 5%% discount is %f", pay);        
+        printf("The amount to be payed after applying 5%% discount is %.2f", pay);        
     }else if(m>7000 && m<=9000){
         float discount = 0.1*m;
         float pay = m-discount;
-        printf("The amount to be payed after applying 10%% discount is %f", pay);        
+        printf("The amount to be payed after applying 10%% discount is %.2f", pay);        
     }else{
         float discount = 0.2*m;
         float pay = m-discount;
-        printf("The amount to be payed after applying 20%% discount is %f", pay);        
+        printf("The amount to be payed after applying 20%% discount is %.2f", pay);        
     }
 }
