ce4c7e87ac38afb0a5db5a046c584de6b6791214
2008adnan <2008adnanahmad@gmail.com>
2026-06-18 19:01:33 +0530

    feat: update practise10 to use float for marks and improve grade calculation

--- Conditional Statements/problems on condn stmt/practise10.c
+++ Conditional Statements/problems on condn stmt/practise10.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 int main(){
-    int s1,s2,s3,s4;
+    float s1,s2,s3,s4;
     printf("enter you marks of all the four subjects:");
-    scanf("%d", &s1);
-    scanf(" %d", &s2);
-    scanf(" %d", &s3);
-    scanf(" %d", &s4);
-    int per = (s1+s2+s3+s4)/4;
-    printf("Percentage = %d %%" , per);
+    scanf("%f", &s1);
+    scanf(" %f", &s2);
+    scanf(" %f", &s3);
+    scanf(" %f", &s4);
+    float per = (s1+s2+s3+s4)/4;
+    char g;
+    if(per<35){
+        g = 'F';
+    }else if(per<45){
+        g = 'E';
+    }else if(per<60){
+        g = 'D';
+    }else if(per<75){
+        g = 'C';
+    }else if(per<90){
+        g = 'B';
+    }else{
+        g = 'A';
+    }
+    printf("Percentage = %.2f%% , Grade: %c",per, g);
+    return 0;
 }
