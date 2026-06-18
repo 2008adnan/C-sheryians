b507edc30709e08724072f79c98e4ea30f2ee794
2008adnan <2008adnanahmad@gmail.com>
2026-06-18 19:25:27 +0530

    practise11

---
+++ Conditional Statements/problems on condn stmt/practise11.c
@@ -0,0 +1,18 @@
+//write a program that takes movie rating as input and tells its status
+#include <stdio.h>
+int main(){
+    float rating;
+    printf("enter your rating for movie:");
+    scanf("%f", &rating);
+    if(rating>= 0 && rating<=2){
+        printf("Flop");
+    }else if(rating>2 && rating<=3.5){
+        printf("Semi-hit");
+    }else if(rating>3.5 && rating<=4.5){
+        printf("Hit");
+    }else if(rating>4.5 && rating<=5){
+        printf("SuperHit");
+    }else{
+        printf("Enter Valid Rating");
+    }
+}
