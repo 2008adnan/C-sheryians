f1d760b5b034a077bb5f7e32baa6b7236a3f6a57
2008adnan <2008adnanahmad@gmail.com>
2026-06-20 18:20:09 +0530

    fghb

---
+++ Conditional Statements/Advanced Control Flow Stmt/problems using switch1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter your character:");
+    scanf("%c",&ch);
+    switch (ch){
+    case 'a':
+    case 'e':        
+    case 'i':        
+    case 'o':    
+    case 'u':
+        printf("Vowel");
+        break;
+    default: printf("Consonant");
+        break;
+    }
+    return 0;
+}
