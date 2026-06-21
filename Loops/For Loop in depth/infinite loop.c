8dedbdcdf011b7d1c2f17240afcae6cde19b34cf
2008adnan <2008adnanahmad@gmail.com>
2026-06-21 19:34:23 +0530

    infinite loop

---
+++ Loops/For Loop in depth/infinite loop.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    for( ; ; ){   //since no condition is specified it never becomes falseand the loop goes till infinite
+        printf("hehe\n");
+    }
+    return 0;
+}
