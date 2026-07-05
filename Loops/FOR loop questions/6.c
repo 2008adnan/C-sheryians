1e16cae0194c8cd4580512b06c8b65f093a43ec9
2008adnan <2008adnanahmad@gmail.com>
2026-07-05 17:00:00 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Loops/FOR loop questions/6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int n=5;
+    int fac=1,sum=0;
+    for(int j = 1;j<=n;j++){
+        fac = 1;
+        for(int i = j; i>=1;i--){
+        fac = fac * i;                          
+        }   
+        if(j!=n){
+            printf("%d! + ", j);
+        } else {
+            printf("%d! = ", j);
+        }
+        sum += fac;
+    }        
+    
+    printf("%d\n", sum);
+    
+    return 0;
+    }
+    
