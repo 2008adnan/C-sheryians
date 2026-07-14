6c32e9a68d3186cc5b3ae48d32893c69c1d94c77
2008adnan <2008adnanahmad@gmail.com>
2026-07-14 17:09:25 +0530

    swap pass by value

--- Functions/Mastering functions/5.c
+++ Functions/Mastering functions/5.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
-int fun(int);  //step 1 - Function Declaration (Prototype)..
+void swap(int,int);  //step 1 - Function Declaration (Prototype)..
 
 int main(){
-    int num = 10;
-    printf("before fxn call %d\n", num);
-    printf("fxn call %d\n", fun(num));  //function call 
-    printf("after fxn call %d\n", num);
+    int x = 10, y = 20;
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+    swap(x, y);  //Function call
+    printf("After swapping: x = %d, y = %d\n", x, y);
     return 0;
 }
 
 //step 2 - Function Definition/implementation
-int fun(int a){
-    a = 100;
-    return a;    
+void swap(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("Inside swap function: a = %d, b = %d\n", a, b);
 }
 
