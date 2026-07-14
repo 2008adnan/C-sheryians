63c035e752ff11a206624fbe2d3dd5a8965b4aa3
2008adnan <2008adnanahmad@gmail.com>
2026-07-14 15:57:31 +0530

    Fix output formatting in demo.c and update binary executable

--- Functions/Mastering functions/demo.c
+++ Functions/Mastering functions/demo.c
@@ -8,6 +8,7 @@ int main(){
     printf("Enter two numbers:");
     scanf("%d %d",&x,&y);
     int ans=sumfun(x,y);
-    printf("Sum is:%d",ans); //we can also write printf("Sum is:%d",sumfun(x,y)); instead of using ans variable.
+    printf("Sum is:%d\n",ans); //we can also write printf("Sum is:%d",sumfun(x,y)); instead of using ans variable.
+    printf("%d\n",sumfun(13,20)); //we can also call the function directly in printf statement.
     return 0;
 }
