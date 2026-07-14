398f5d0b5dfe4d2a8c699277b782d23aa6e7b0b1
2008adnan <2008adnanahmad@gmail.com>
2026-07-14 16:20:54 +0530

    return

--- Functions/Mastering functions/2.c
+++ Functions/Mastering functions/2.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
-
-int sumfun(int a,int b){
-    return a+b;
-}
+void printhello();  //step 1 - Function Declaration (Prototype)...always declare the function before using it in main function.
 
 int main(){
-    int x,y;
-    printf("Enter two numbers:");
-    scanf("%d %d",&x,&y);
-    int ans=sumfun(x,y);
-    printf("Sum is:%d\n",ans); //we can also write printf("Sum is:%d",sumfun(x,y)); instead of using ans variable.
-    printf("%d\n",sumfun(13,20)); //we can also call the function directly in printf statement.
+    printhello();  // step 3 - Function Call
     return 0;
 }
 
+//step 2 - Function Definition/implementation
+void printhello(){
+    for(int i=0;i<5;i++){
+        printf("Hello\n");
+    }
+}
 
