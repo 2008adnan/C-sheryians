8ba3b5d9a3ec27bf944b1d87c8cea34d511353a0
2008adnan <2008adnanahmad@gmail.com>
2026-07-17 11:14:26 +0530

    fact

---
+++ Functions/Recursion Explained/3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int factorial(int n){
+    if (n == 0) {
+        return 1; // Base case: factorial of 0 is 1
+    }
+
+        return n * factorial(n - 1); // Recursive case
+    }
+
+int main(){
+    int n;
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+    printf("factorial %d", factorial(n));
+    return 0;
+}    
