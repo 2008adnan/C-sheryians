0ac2974c176a3bc3050221f74f6e2d060d4937b0
2008adnan <2008adnanahmad@gmail.com>
2026-07-19 15:50:32 +0530

    learning how to put value in an array

---
+++ Array - Introduction to DS/Introduction to Array/demo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int arr[3];
+    arr[0] = 10;
+    arr[1] = 13;
+    arr[2] = 17;
+    printf("%d\n",arr[0]);
+    printf("%d\n",arr[1]);
+    printf("%d\n",arr[2]);
+
+    int array[5] = {1,2,3,4,5};
+    printf("%d\n",array[0]);
+    printf("%d\n",array[1]);
+    printf("%d\n",array[2]);
+    printf("%d\n",array[3]);
+    printf("%d\n",array[4]);
+    return 0;
+}
