f6b35f41591e6e86605d9dbf04561b2fedd43667
2008adnan <2008adnanahmad@gmail.com>
2026-08-09 11:48:28 +0530

    updated

--- Array - Introduction to DS/Array In Depth/left rotate array by 1.c
+++ Array - Introduction to DS/Array In Depth/left rotate array by 1.c
@@ -8,11 +8,14 @@ int main(){
         printf("Enter arr[%d] element: \n",i);
         scanf("%d", &arr[i]);
     }
+    //store the first element in a temporary variable
     int temp = arr[0];
+    //shift all elements to the left by one position
     for(int i=0;i<size-1;i++){
         arr[i] = arr[i+1];
     }
-    arr[size-1] = temp;
+    arr[size-1] = temp; //store the first element in the last position of the array
+    //print the rotated array
     for(int i=0;i<size;i++){
         printf("%d ", arr[i]);
     }
