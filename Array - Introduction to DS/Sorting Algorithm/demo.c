1d648f699ddf5e6396c25a88b0ebb75b232f47aa
2008adnan <2008adnanahmad@gmail.com>
2026-08-13 13:17:36 +0530

    bubble sort

--- Array - Introduction to DS/Sorting Algorithm/demo.c
+++ Array - Introduction to DS/Sorting Algorithm/demo.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 int main(){
-    
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int a[size];
+    printf("Enter the elements of the array: ");
+    for(int i=0; i<size; i++){
+        printf("Element a[%d]:\n", i);
+        scanf("%d", &a[i]);
+    }
+    for(int i=0; i<size-1;i++){
+        for(int j= 0;j<size-1-i;j++){
+        if(a[j]>a[j+1]){
+            int temp = a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+        }
+    }
+    }
+    for(int i=0;i<size;i++){
+        printf("%d ",a[i]);
+    }
     return 0;
-}
+} 
