bb5b5947dc5a8ae7c4d293d036b8dfb112849270
2008adnan <2008adnanahmad@gmail.com>
2026-08-17 15:07:35 +0530

    reading string input

--- String - Introduction to DS/demo.c
+++ String - Introduction to DS/demo.c
@@ -4,9 +4,19 @@ int main(){
     char str2[] = "Hello, World!";
     char str3[] = {'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
     char str4[14] = {'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
+
+    // updating the string characters
+    str[7] = 'C';
+    str2[7] = 'C';
+
+
     printf("%s\n", str);
     printf("%s\n", str2);
     printf("%s\n", str3);
     printf("%s\n", str4);
+
+    //access ing individual characters
+    printf("%c\n", str[0]);
+    printf("%c\n", str2[1]);
     return 0;
 }
---
+++ String - Introduction to DS/reading string input.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    char name[50];
+    //using scanf
+        // printf("Enter your name: ");
+        // scanf("%s", name);
+        // printf("%s\n", name);
+    // //using gets()
+        // char str2[54];
+        // printf("Enter your name: ");
+        // gets(str2);
+        // printf("%s\n", str2);
+    //using fgets()
+        // char str3[54];
+        // printf("Enter your name: ");
+        // fgets(str3,54,stdin);
+        // printf("%s\n", str3);
+    //using scanf with scanset
+        // char str[54];
+        // printf("Enter your name: ");
+        // scanf("%[^\n]s", str);
+        // printf("%s\n", str);
+    return 0;
+}
